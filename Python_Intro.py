62df1dc5a2f25544cc9376b7a36a2e54e4ce3b5b
shekhar270779 <p.shekhar.2707@gmail.com>
2019-11-08 20:56:36 +0530

    added time module commands to get local time

--- Python_Intro.py
+++ Python_Intro.py
@@ -1,4 +1,10 @@
 import sys
+import time
 
 print(sys.version) # to find python version
 print("Hello and Welcome to Python")
+
+seconds = time.time() # returns no. of seconds passed since epoch
+local_time = time.ctime(seconds) # returns local time
+print(local_time)
+
